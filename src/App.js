0da0658e59ce1115f25496007ebbd3a58e62db5b
steviesocks <stephen.stocking@gmail.com>
2020-12-18 10:52:04 -0500

    first working version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React, { useRef, useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './App.css';
 import Gallery from './components/gallery/gallery.component';
 import Search from './components/search/search.component';
 
+import { getAPI } from './utils/api';
+
 const App = () => {
+
+  const offset = useRef(0);
+  const endpoint = useRef("trending")
+  console.log('offset', offset.current)
 
   const [search, setSearch] = useState("")
   const [gifs, setGifs] = useState([])
+  const [url, setUrl] = useState(`https://api.giphy.com/v1/gifs/trending?api_key=KU304SCxltM7TA3lecV28vBV0Rv4A5XE&limit=9`)
 
-  const endpoint = useRef("trending")
+  console.log(search)
 
   const handleChange = (event) => {
     const { value } = event.target;
     setSearch(value)
   }
 
-  const handleSearch = () => {
-    setGifs([])
+  const handleSearch = async () => {
+    const newUrl = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=KU304SCxltM7TA3lecV28vBV0Rv4A5XE&limit=9`
+    setUrl(newUrl)
+
+    getAPI(`${newUrl}&offset=0`)
+            .then(response => setGifs([...response.data]))
+            .catch(error => console.log(error))
+
+    offset.current = 9
     endpoint.current = "search"
+
   }
+
+  useEffect(() => {
+    getAPI(`https://api.giphy.com/v1/gifs/trending?api_key=KU304SCxltM7TA3lecV28vBV0Rv4A5XE&limit=9&offset=0`)
+            .then(response => setGifs([...response.data]))
+            .catch(error => console.log(error))
+
+    offset.current = 9
+  }, [])
 
   return (
     <div className="App">
@@ -26,7 +49,18 @@ const App = () => {
         <h1>Giphy scroller</h1>
       </header>
       <Search handleChange={handleChange} defaultValue="search giphy..." handleSearch={handleSearch} />
-      <Gallery endpoint={endpoint.current} gifs={gifs} setGifs={setGifs} query={search}/>
+      <Gallery 
+        url={url} 
+        setUrl={setUrl} 
+        offset={offset} 
+        gifs={gifs} 
+        setGifs={setGifs} 
+        title={
+            endpoint.current === "trending" ?
+              "what's trending now" :
+              `top results for "${search}"`
+          }
+        />
     </div>
   );
 }
